e03dac0205ee1e616b4d23a8ecb53c17f75cb735
Austin <asibik2@gmail.com>
2025-07-29 15:06:08 -0500

    reworded

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ export default function ProcessCostCalculator() {
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required />
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
               </div>
-              <button type="submit">Send my Result</button>
+              <button type="submit">View my Result</button>
             </form>
           </div>
         </>
